433b900d1694efe2ccc16d2ac04192ed1431389a
Kellyuliana23 <kellygarcia143691@correo.itm.edu.co>
2018-03-07 23:48:46 -0500

    LoginViewModel and BaseViewModel

--- bibles/Bibles/Bibles/ViewModels/LoginViewModel.cs
+++ bibles/Bibles/Bibles/ViewModels/LoginViewModel.cs
@@ -1,12 +1,26 @@
 ﻿namespace Bibles.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
     using System.Windows.Input;
-   
-    public class LoginViewModel
+    using Xamarin.Forms;
+
+    //Heredar de la BaseViewModel para evitarme crear tantos eventos
+    public class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+        //Atributo privado con inicial en minúscula
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+
         //código (video 7)
-     
+
         #region Properties
+        //atributo público con inicial en mayúscula
+
         public string Email
         {
             get;
@@ -15,20 +29,25 @@
 
         public string Password
         {
-            get;
-            set;
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
         }
-
         public bool IsRemembered
         {
             get;
             set;
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
         }
         #endregion
 
@@ -36,6 +55,7 @@
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
 
         }
         #endregion
@@ -43,8 +63,60 @@
         #region Commands 
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                //uso el nuget que adicionamos mvvmLightLibs
+                return new RelayCommand(Login);
+            }
+        }
+
+
+        private async void Login()
+        {
+            //validar que el usr haya digitado email y pass
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email",
+                    "Accept");
+                //Languages.Error,
+                //Languages.EmailValidation,
+                //Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Password",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = true;
+            this.isEnabled = false;
+
+
+            //código quemado, se retira después
+            if (this.Email != "kellyuliana23@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.isEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "OK",
+                    "Welcome!!!",
+                    "Accept");
         }
         #endregion
 
